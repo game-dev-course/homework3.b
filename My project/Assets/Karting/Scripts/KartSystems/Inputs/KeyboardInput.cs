dd166cd01adce052d16d373b37d43bd9832a88d3
oa1321 <74298257+oa1321@users.noreply.github.com>
2023-03-28 00:07:14 +0300

    whoop
    whoop

---
+++ My project/Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace KartGame.KartSystems {
+
+    public class KeyboardInput : BaseInput
+    {
+        public string TurnInputName = "Vertical";
+        public string AccelerateButtonName = "Accelerate";
+        public string BrakeButtonName = "Brake";
+
+        public override InputData GenerateInput() {
+            string a = TurnInputName;
+            return new InputData
+            {
+                Accelerate = Input.GetButton(AccelerateButtonName),
+                Brake = Input.GetButton(BrakeButtonName),
+                TurnInput = Input.GetAxis(a)
+            };
+        }
+    }
+}
